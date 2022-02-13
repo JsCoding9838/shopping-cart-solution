91239a3b90c827a12ff9983346c33e69ef608679
Jubayer Hosen <jubayerhosain9838@gmail.com>
2022-02-13 13:33:31 +0800

    increasing nad decreasing up to date

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log('I am in a separate file');
+function updateCaseNumber(isIncreasing){
+    const caseInput  = document.getElementById('case-number');
+    let caseNumber   = caseInput.value;
+    
+    if(isIncreasing == true){
+        caseNumber  = parseInt(caseNumber) + 1;
+    }
+    else if(caseNumber > 0){
+        caseNumber  = parseInt(caseNumber) - 1;
+    }
+    caseInput.value = caseNumber;
+
+    // update case total
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = caseNumber * 59;
+}
+
+
+document.getElementById('case-plus').addEventListener('click', function(){
+    // const caseInput  = document.getElementById('case-number');
+    // const caseNumber = caseInput.value;
+    // caseInput.value  = parseInt(caseNumber) + 1;
+    // console.log(caseNumber);
+    updateCaseNumber(true);
+})
+document.getElementById('case-minus').addEventListener('click', function (){
+    // const caseInput  = document.getElementById('case-number');
+    // const caseNumber = caseInput.value;
+    // caseInput.value  = parseInt(caseNumber) - 1;
+    updateCaseNumber(false);
+})
