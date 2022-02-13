a4d3033fed4fbc2f5d3d901156db5c5c3e685544
Jubayer Hosen <jubayerhosain9838@gmail.com>
2022-02-13 14:21:24 +0800

    all function updated

--- app.js
+++ app.js
@@ -1,31 +1,43 @@
-function updateCaseNumber(isIncreasing){
-    const caseInput  = document.getElementById('case-number');
-    let caseNumber   = caseInput.value;
-    
-    if(isIncreasing == true){
-        caseNumber  = parseInt(caseNumber) + 1;
-    }
-    else if(caseNumber > 0){
-        caseNumber  = parseInt(caseNumber) - 1;
-    }
-    caseInput.value = caseNumber;
+function updateProductNumber(product, price, isIncreasing){
+    const productInput  = document.getElementById(product + '-number');
+    let productNumber   = productInput.value;
 
-    // update case total
-    const caseTotal = document.getElementById('case-total');
-    caseTotal.innerText = caseNumber * 59;
+    if(isIncreasing == true){
+        productNumber  = parseInt(productNumber) + 1;
+    }
+    else if(productNumber > 0){
+        productNumber  = parseInt(productNumber) - 1;
+    }
+    productInput.value = productNumber;
+
+    // update  total
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productNumber * price;
 }
 
 
-document.getElementById('case-plus').addEventListener('click', function(){
+// phone increase decrease events
+document.getElementById('phone-plus').addEventListener('click', function (){
+    updateProductNumber('phone', 1219, true);
+})
+document.getElementById('phone-minus').addEventListener('click', function (){
+    updateProductNumber('phone', 1219, false);
+})
+
+
+
+
+// handle case increase decrease events
+document.getElementById('case-plus').addEventListener('click', function (){
     // const caseInput  = document.getElementById('case-number');
     // const caseNumber = caseInput.value;
     // caseInput.value  = parseInt(caseNumber) + 1;
     // console.log(caseNumber);
-    updateCaseNumber(true);
+    updateProductNumber('case', 59, true);
 })
 document.getElementById('case-minus').addEventListener('click', function (){
     // const caseInput  = document.getElementById('case-number');
     // const caseNumber = caseInput.value;
     // caseInput.value  = parseInt(caseNumber) - 1;
-    updateCaseNumber(false);
+    updateProductNumber('case', 59, false);
 })
